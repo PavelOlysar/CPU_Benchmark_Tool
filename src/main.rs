0e74ee76fc5baa9a63bb1ee886923c78a3901e98
Pavel <olysarp@gmail.com>
2024-07-03 08:19:26 +0200

    calculating factorials

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+//use inditicatif::ProgressBar;
+
+// function to calculate factorial of a number 
+pub fn calculate_factorial(num: u128) -> u128 {
+    (1..=num).product() // calculate factorial of a number
+}
+
+// function to loop and calculate factorial of a number as many times as specified
+fn add_loop(&n_loops: &u64) {
+    for _in in 0..n_loops {
+        let _ = calculate_factorial(20);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    
 }
