8c2c68b766c44e7a3fdd9cfd4a608ee3407bca2c
Pavel <olysarp@gmail.com>
2024-07-04 07:26:03 +0200

    finished

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ pub fn calculate_factorial(num: u128) -> u128 {
 // function to loop and calculate factorial of a number as many times as specified
 fn add_loop(&n_loops: &u64) {
     for _in in 0..n_loops {
-        let _ = calculate_factorial(20);
+        let _ = calculate_factorial(25);
     }
 }
 
